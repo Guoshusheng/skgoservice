1b680af7ab5c498111b2de9e773aed00d439c7ea
SmallTiger <15656560556@163.com>
2017-12-22 16:28:49 +0800

    modify the returned value
    encode the return value

--- server/handle/login.go
+++ server/handle/login.go
@@ -1,12 +1,19 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func GetLoginGuo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "HELLO MAN, Guo Shusheng!")
+	//	fmt.Fprint(w, "HELLO MAN, Guo Shusheng!")
+	json.NewEncoder(w).Encode(
+		struct {
+			Name string `json:"name"`
+		}{
+			Name: "guo shusheng",
+		})
 }
 
 func GetLoginXin(w http.ResponseWriter, r *http.Request) {
