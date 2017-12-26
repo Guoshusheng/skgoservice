e7a8661ef8092cc8fd92a1b55ad78366c2ed72c9
SmallTiger <15656560556@163.com>
2017-12-26 14:04:32 +0800

    add some test data
    add a test record.Specifically, this action allows you to view  the database file

--- server/handle/login.go
+++ server/handle/login.go
@@ -4,18 +4,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"../datastore"
+	"github.com/goji/context"
+	"github.com/zenazn/goji/web"
 )
 
-func GetLoginGuo(w http.ResponseWriter, r *http.Request) {
+func GetLoginGuo(c web.C, w http.ResponseWriter, r *http.Request) {
 	//	fmt.Fprint(w, "HELLO MAN, Guo Shusheng!")
+	var ctx = context.FromC(c)
+	var id int64 = 1
+	testsk, _ := datastore.GetTestSKID(ctx, id)
+	fmt.Fprint(w, testsk)
+}
+
+func GetLoginXin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "HELLO MAN, Xin Tingkai!")
 	json.NewEncoder(w).Encode(
 		struct {
 			Name string `json:"name"`
 		}{
 			Name: "guo shusheng",
 		})
-}
-
-func GetLoginXin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "HELLO MAN, Xin Tingkai!")
 }
--- share/model/testsk.go
+++ share/model/testsk.go
@@ -4,7 +4,7 @@ package model
 
 type TestSK struct {
 	ID          int64  `meddler:"testsk_id,pk"	json:"-"`
-	TestTime    string `meddler:"testsk_time	json:"testsk_time"`
+	TestTime    string `meddler:"testsk_time"	json:"testsk_time"`
 	TestAddress string `meddler:"testsk_add"	json:"testsk_add"`
 	TestName    string `meddler:"testsk_name"	json:"testsk_name"`
 	TestCount   string `meddler:"testsk_count"	json:"testsk_count"`
